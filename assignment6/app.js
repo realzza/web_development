e40a48f2cc50a151335b7bb86751e1f5a8f73e02
ziang <zz188@duke.edu>
2021-10-14 19:27:31 -0400

    quest for user info

--- assignment6/app.js
+++ assignment6/app.js
@@ -16,12 +16,56 @@ gitHubForm.addEventListener("submit", (e) => {
   requestUserRepos(gitHubUsername);
 });
 
+// function requestUserRepos(username) {
+//   // Create new XMLHttpRequest object
+//   const xhr = new XMLHttpRequest();
+
+//   // GitHub endpoint, dynamically passing in specified username
+//   const url = `https://api.github.com/users/${username}/repos`;
+
+//   // Open a new connection, using a GET request via URL endpoint
+//   // Providing 3 arguments (GET/POST, The URL, Async True/False)
+//   xhr.open("GET", url, true);
+
+//   // When request is received
+//   // Process it here
+//   xhr.onload = function () {
+//     // Parse API data into JSON
+//     const data = JSON.parse(this.response);
+
+//     // Loop over each object in data array
+//     for (let i in data) {
+//       // Get the ul with id of of userRepos
+//       let ul = document.getElementById("userRepos");
+
+//       // Create variable that will create li's to be added to ul
+//       let li = document.createElement("li");
+
+//       // Add Bootstrap list item class to each li
+//       li.classList.add("list-group-item");
+
+//       // Create the html markup for each li
+//       li.innerHTML = `
+//                 <p><strong>Repo:</strong> ${data[i].name}</p>
+//                 <p><strong>Description:</strong> ${data[i].description}</p>
+//                 <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+//             `;
+
+//       // Append each li to the ul
+//       ul.appendChild(li);
+//     }
+//   };
+
+//   // Send the request to the server
+//   xhr.send();
+// }
+
 function requestUserRepos(username) {
   // Create new XMLHttpRequest object
   const xhr = new XMLHttpRequest();
 
   // GitHub endpoint, dynamically passing in specified username
-  const url = `https://api.github.com/users/${username}/repos`;
+  const url = `https://api.github.com/users/${username}`;
 
   // Open a new connection, using a GET request via URL endpoint
   // Providing 3 arguments (GET/POST, The URL, Async True/False)
@@ -34,26 +78,28 @@ function requestUserRepos(username) {
     const data = JSON.parse(this.response);
 
     // Loop over each object in data array
-    for (let i in data) {
-      // Get the ul with id of of userRepos
-      let ul = document.getElementById("userRepos");
+    // for (let i in data) {
+    // Get the ul with id of of userRepos
+    let ul = document.getElementById("userRepos");
 
-      // Create variable that will create li's to be added to ul
-      let li = document.createElement("li");
+    // Create variable that will create li's to be added to ul
+    let li = document.createElement("li");
 
-      // Add Bootstrap list item class to each li
-      li.classList.add("list-group-item");
+    // Add Bootstrap list item class to each li
+    li.classList.add("list-group-item");
 
-      // Create the html markup for each li
-      li.innerHTML = `
-                <p><strong>Repo:</strong> ${data[i].name}</p>
-                <p><strong>Description:</strong> ${data[i].description}</p>
-                <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+    // Create the html markup for each li
+    li.innerHTML = `
+                <p><strong>User Name:</strong> ${data.login}</p>
+                <p><strong>Company:</strong> ${data.company}</p>
+                <p><strong>Description:</strong> ${data.bio}</p>
+                <p><strong>Followers:</strong> ${data.followers}</p>
+                <p><strong>URL:</strong> <a href="${data.html_url}">${data.html_url}</a></p>
             `;
 
-      // Append each li to the ul
-      ul.appendChild(li);
-    }
+    // Append each li to the ul
+    ul.appendChild(li);
+    // }
   };
 
   // Send the request to the server
